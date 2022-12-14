e0dfe24351e195cefa3b495736db7612fe9bc82a
Rodrigo1331 <117907875+Rodrigo1331@users.noreply.github.com>
2022-12-14 07:23:16 -0800

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,43 +6,55 @@
 /*   By: rcruz-an <rcruz-an@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 12:06:19 by rcruz-an          #+#    #+#             */
-/*   Updated: 2022/12/14 10:26:58 by rcruz-an         ###   ########.fr       */
+/*   Updated: 2022/12/14 15:22:48 by rcruz-an         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ReadLine(char *s, int fd) // Read, and creat the string until it reaches \n or NULL
+char	*RecreatBuff(int fd, char t) // recreat malloc and free the old stach
 {
-	char	*buff;
-	char	*str;
-	int		t;
+	char	*new_stash;
+	int		i;
+	int		j;
 	
-	buff = (char *)malloc(sizeof((char) * BUFFER_SIZE + 1));
-	t = read(fd, buff, BUFF_SIZE);
-	if (!t)
-	{
-		free (buff);
-		return (0);
-	}
 	
-
-	// add null terminator
-	free (buff); // probably not recommended
-	return (ft_strjoin(buff, str));
-}
-
-char	*StringAfterNewLine(int fd, char t) // string after the \n or/and NULL (+2)
-{
-	 
 	free (t);	
 }
 
 char	*EndLine(int fd, char *t) //search for \n and NULL
 {
-	
+	int	len;
+	int	i;
 
+	i = 0;
+	//len = ft_strlen(char *t)
+	while (t[i] != '\0' && t[i] != '\n')
+		i++;
+	return (i);
+}
+
+char	*ReadLine(char *s, int fd) // Read and concatenate the string until it reaches \n or NULL
+{
+	char	*buff;
+	char	*str;
+	int		t;
 	
+	s = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	buff = (char *)malloc(sizeof((char) * BUFFER_SIZE + 1));
+	t = read(fd, buff, BUFF_SIZE);
+	if (!t)
+	{
+		free (s);
+		free (buff);
+		return (0);
+	}
+	if (endline(int fd, char *s) == BUFFER_SIZE) //probably not ok
+
+
+	// add null terminator
+	free (buff); // probably not recommended
+	return (ft_strjoin(buff, str));
 }
 
 char	*get_next_line(int fd)
@@ -59,9 +71,25 @@ char	*get_next_line(int fd)
 
 }
 
-/* int main(void){
-	int fd = "text.txt";
+/*
+int	main(void)
+{
+	char	*line;
+	int		i;
+	int		fd;
 
-	printf("", get_next_line(fd))
-
+	fd = open("teste.txt", O_RDONLY);
+	i = 1;
+	while (fd)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break;
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	free (line);
+	close(fd);
+	return (0);
 }*/
